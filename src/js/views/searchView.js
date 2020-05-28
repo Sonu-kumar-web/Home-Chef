faf4f6a0df7f23d83e523cf5fae576e93b1b09ee
Sonu <sonu3660@gmail.com>
2020-05-28 22:43:38 +0530

    Building the search view

--- src/js/views/searchView.js
+++ src/js/views/searchView.js
@@ -3,14 +3,32 @@ import {elements} from './base';
 export const getInput = () => elements.searchInput.value;
 
 export const clearInput = () => {
+
+    // Clear the search space after every search (or result )
     elements.searchInput.value = '';
 };
 
 export const clearResults = () => {
 
-    // Remove all element from markup 
+    // Remove all element from markup before search next result
     elements.searchResList.innerHTML = '';
 
+};
+
+// Reduce the title size followed by three dots without cut any word
+const limitRecipeTitle = (title, limit = 17) => {
+    const newTitle = [];
+    if(title.length > limit){
+        title.split(' ').reduce((acc, cur) => {
+            if(acc + cur.length <= limit){
+                newTitle.push(cur);
+            }
+            acc = acc + cur.length;
+        }, 0);
+        // return the result
+        return `${newTitle.join(' ')} ...`;
+    }
+    return title;
 };
 
 const renderRecipes = recipe => {
@@ -20,7 +38,7 @@ const renderRecipes = recipe => {
                                 <img src="${recipe.image_url}" alt="${recipe.title}">
                             </figure>
                             <div class="results__data">
-                                <h4 class="results__name">${recipe.title}</h4>
+                                <h4 class="results__name">${limitRecipeTitle(recipe.title)}</h4>
                                 <p class="results__author">${recipe.publisher}</p>
                             </div>
                         </a>
