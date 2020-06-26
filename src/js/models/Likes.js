bac094553dbf9e21877356f082f47c37f93b3d53
Sonu <sonu3660@gmail.com>
2020-06-27 01:39:49 +0530

    Building the Likes Model

---
+++ src/js/models/Likes.js
@@ -0,0 +1,17 @@
+export default class Likes {
+    constructor () {
+        this.likes = [];
+    }
+    addLike(id, title, author, img) {
+        const like = { id, title, author, img };
+        this.likes.push(like);
+        return like;
+    }
+
+    deleteLike (id) {
+        const index = this.likes.findIndex(el => el.id === id);
+        // [2,4,8] splice(1,1) -> returns 4, original array is [2,8] (mutate original array).
+        // [2,4,8] splice(1,1) -> returns 4, original array is [2,4,8] (do not mutate original array).
+        this.likes.splice(index, 1);
+    }
+}
